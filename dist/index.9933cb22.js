7c937b1d9de1dc33d20e5c13fabc267e46137941
utopia-code <utopiacode.developer@gmail.com>
2022-11-14 22:57:39 +0100

    add bio container-flex

---
+++ dist/index.9933cb22.js
@@ -0,0 +1,2 @@
+!function(){console.log("Hello world!");let e=document.getElementsByClassName("burger-menu");e.addEventListener("click",(function(){e.classList.toggle("change")}))}();
+//# sourceMappingURL=index.9933cb22.js.map
