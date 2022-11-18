e3d58bb497db545ba3ad532e68a27133cdedb48c
utopia-code <utopiacode.developer@gmail.com>
2022-11-18 21:44:37 +0100

    special typo testing

--- src/js/app.js
+++ src/js/app.js
@@ -1,53 +1,33 @@
 /* Laura López Doval */
 
-console.log('Hello world!');
-
 // add active class navigation based on url -> https://stackoverflow.com/questions/20060467/add-active-navigation-class-based-on-url
 
-// $(function(){
-//     var current = location.pathname;
-//     $('#nav li a').each(function(){
-//         var $this = $(this);
-//         // if the current path is like this link, make it active
-//         if($this.attr('href').indexOf(current) !== -1){
-//             $this.addClass('active');
-//         }
-//     })
-// })
-
-// jQuery(function($) {
-//     var path = window.location.href; // because the 'href' property of the DOM element is the absolute path
-//     $('ul a').each(function() {
-//      if (this.href === path) {
-//       $(this).addClass('active');
-//      }
-//     });
-//    });
-
-// (function () {
-//     var current = location.pathname.split('/')[1];
-//     if (current === "") return;
-//     var menuItems = document.querySelectorAll('.menu-item a');
-//     for (var i = 0, len = menuItems.length; i < len; i++) {
-//         if (menuItems[i].getAttribute("href").indexOf(current) !== -1) {
-//             menuItems[i].className += "is-active";
-//         }
-//     }
-// })();
+(function () {
+    var current = location.pathname.split('/')[1];
+    if (current === "") return;
+    var menuItems = document.querySelectorAll('.menu-item a');
+    for (var i = 0, l = menuItems.length; i < l; i++) {
+        if (menuItems[i].getAttribute('href').indexOf(current) !== -1) {
+            menuItems[i].classList.add('is-active');
+        }
+    }
+})();
 
 
 // burger menu
 // https://www.w3schools.com/howto/tryit.asp?filename=tryhow_css_menu_icon_js 
 
-let burger_menu = document.querySelector('.burger-menu');
-let nav = document.querySelector('.nav');
-let content = document.querySelector('.header-collapsed');
+(function () {
+    let burger_menu = document.querySelector('.burger-menu');
+    let nav = document.querySelector('.nav');
+    let content = document.querySelector('.header-collapsed');
 
-burger_menu.addEventListener('click', function() {
-    burger_menu.classList.toggle('change');
-    nav.classList.toggle('collapsed');
-    content.classList.toggle('top-to-header');
-})
+    burger_menu.addEventListener('click', function() {
+        burger_menu.classList.toggle('change');
+        nav.classList.toggle('collapsed');
+        content.classList.toggle('top-to-header');
+    })
+})();
 
 // infinity carousel
 // https://codepen.io/hmdshfq/pen/JjrZNgP
