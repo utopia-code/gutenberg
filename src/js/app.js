a6d1ccd6b387030ff50f8226664dec897bf37504
utopia-code <utopiacode.developer@gmail.com>
2022-11-21 19:43:56 +0100

    Task -> biography site

--- src/js/app.js
+++ src/js/app.js
@@ -1,20 +1,45 @@
 /* Laura López Doval */
 
 // sticky sidebar
+// https://webdevetc.com/blog/matchmedia-events-for-window-resizes/
 
 import './resize-sensor.js';
 import './sticky-sidebar.js';
 
 (function () {
-    var sidebar = document.querySelectorAll('.sidebar');
-    sidebar.forEach(function(s) {
-        new StickySidebar(s, {
-            topSpacing: 100,
-            bottomSpacing: 20,
-            containerSelector: '.main-content',
-            innerWrapperSelector: '.sidebar__inner'
-        });
-    })
+    const mediaQuery = window.matchMedia('(min-width: 992px)');
+    function handleDesktopChange(e) {
+        if (e.matches) {
+            var sidebar = document.querySelectorAll('.sidebar');
+            sidebar.forEach(function(s) {
+                new StickySidebar(s, {
+                    topSpacing: 100,
+                    bottomSpacing: 20,
+                    containerSelector: '.main-content',
+                    innerWrapperSelector: '.sidebar__inner'
+                });
+            })
+        }
+    }
+    mediaQuery.addListener(handleDesktopChange);
+    handleDesktopChange(mediaQuery);
+
+    // const mediaQuery = '(max-width: 700px)';
+    // const mediaQueryList = window.matchMedia(mediaQuery);
+
+    // window.addEventListener('resize', event => {
+    //     if (window.innerWidth <= 992) {
+    //         var sidebar = document.querySelectorAll('.sidebar');
+    //         sidebar.forEach(function(s) {
+    //             new StickySidebar(s, {
+    //                 topSpacing: 100,
+    //                 bottomSpacing: 20,
+    //                 containerSelector: '.main-content',
+    //                 innerWrapperSelector: '.sidebar__inner'
+    //             });
+    //         })
+    //     }
+    // })
 })();
 
 // add active class navigation based on url -> https://stackoverflow.com/questions/20060467/add-active-navigation-class-based-on-url
